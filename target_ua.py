7f4c31c600363f5e1aa6c4f07fc47ca0c0157735
MatthewPr12 <matvii.prytula@ucu.edu.ua>
2021-10-30 17:39:26 +0300

    added checking func & improved with pylint

--- target_ua.py
+++ target_ua.py
@@ -15,20 +15,40 @@ def generate_grid():
 
 def get_words(file_name, letters):
     """gets words from the file and puts it in the dic"""
-    dict_of_words = {}
+    dic_of_words = {}
     parts_of_speech = ["/n", "noun", "adj", "/v", "verb", "adv", ]
-    with open(file_name, "r") as file_ukr:
+    with open(file_name, "r", encoding='utf-8') as file_ukr:
         for line in file_ukr:
             line = line.split()
             if len(line[0]) <= 5 and line[0][0] in letters:
                 for i in parts_of_speech:
                     if i in line[1]:
-                        dict_of_words[line[0]] = i
-    return dict_of_words
+                        if i in ('/n', 'noun'):
+                            dic_of_words[line[0]] = "noun"
+                        elif i in ('/v', 'verb'):
+                            dic_of_words[line[0]] = "verb"
+                        elif i == "adj":
+                            dic_of_words[line[0]] = "adjective"
+                        elif i == "adv":
+                            dic_of_words[line[0]] = "adverb"
+    return dic_of_words
 
 
 def check_user_words(user_words, language_part, letters, dict_of_words):
-    pass
+    """checks the users words"""
+    right_words = []
+    lst_of_words = list(dict_of_words.keys())
+    for i in user_words:
+        if i[0].lower() in letters:
+            if i in lst_of_words:
+                if dict_of_words[i] == language_part:
+                    right_words.append(i)
+                lst_of_words.remove(i)
+    return right_words, lst_of_words
 
-print(generate_grid())
-print(get_words("base.lst", ["а", "м", "п", "с", "у"]))
+
+# if __name__ == "__main__":
+#     letters = generate_grid()
+#     dict_of_words = get_words("base.lst", letters)
+#     print(letters)
+#     print(dict_of_words)
