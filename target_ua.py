902b9be43e9ef190e89e8abb03eff7820b626463
MatthewPr12 <matvii.prytula@ucu.edu.ua>
2021-10-30 19:55:40 +0300

    switched from dicts to lists to add duplicates

--- target_ua.py
+++ target_ua.py
@@ -15,40 +15,60 @@ def generate_grid():
 
 def get_words(file_name, letters):
     """gets words from the file and puts it in the dic"""
-    dic_of_words = {}
+    dic_of_words = []
     parts_of_speech = ["/n", "noun", "adj", "/v", "verb", "adv", ]
     with open(file_name, "r", encoding='utf-8') as file_ukr:
         for line in file_ukr:
             line = line.split()
             if len(line[0]) <= 5 and line[0][0] in letters:
                 for i in parts_of_speech:
-                    if i in line[1]:
+                    if i in line[1][:5]:
                         if i in ('/n', 'noun'):
-                            dic_of_words[line[0]] = "noun"
+                            dic_of_words.append((line[0], "noun"))
                         elif i in ('/v', 'verb'):
-                            dic_of_words[line[0]] = "verb"
+                            dic_of_words.append((line[0], "verb"))
                         elif i == "adj":
-                            dic_of_words[line[0]] = "adjective"
+                            dic_of_words.append((line[0], "adjective"))
                         elif i == "adv":
-                            dic_of_words[line[0]] = "adverb"
+                            dic_of_words.append((line[0], "adverb"))
     return dic_of_words
 
 
 def check_user_words(user_words, language_part, letters, dict_of_words):
     """checks the users words"""
     right_words = []
-    lst_of_words = list(dict_of_words.keys())
+    missed_words = []
+    lst_of_words = []
+    lst_of_parts = []
+    for i in dict_of_words:
+        lst_of_words.append(i[0])
+        lst_of_parts.append(i[1])
     for i in user_words:
+        two_parts = []
         if i[0].lower() in letters:
             if i in lst_of_words:
-                if dict_of_words[i] == language_part:
+                if lst_of_parts[lst_of_words.index(i)] == language_part:
                     right_words.append(i)
-                lst_of_words.remove(i)
-    return right_words, lst_of_words
+                elif lst_of_words[lst_of_words.index(i)] == lst_of_words[lst_of_words.index(i) + 1]:
+                    two_parts.append(lst_of_parts[lst_of_words.index(i)])
+                    two_parts.append(lst_of_parts[lst_of_words.index(i) + 1])
+                    if language_part in two_parts:
+                        right_words.append(i)
+
+    for i in range(len(lst_of_words)):
+        if lst_of_parts[i] == language_part and lst_of_words[i] not in right_words:
+            missed_words.append(lst_of_words[i])
+    return right_words, missed_words
 
 
-# if __name__ == "__main__":
-#     letters = generate_grid()
-#     dict_of_words = get_words("base.lst", letters)
-#     print(letters)
-#     print(dict_of_words)
+if __name__ == "__main__":
+    # print(sorted(set(get_words("base.lst", ['щ']))))
+    # print(sorted(set(get_words("base.lst", ['й', 'є', 'ю']))))
+    # print(check_user_words([], "verb", ['щ', 'ш', 'ь', 'у', 'ц'], get_words("base.lst", ['щ', 'ш', 'ь', 'у', 'ц'])))
+    # print(get_words("base.lst", ['ф', 'у', 'щ', 'б', 'л']))
+    print(check_user_words(['гаяти', 'гнати', 'ініціалізація', 'узяти', 'щавель'], "verb", ['ю', 'щ', 'я', 'ц', 'г'], get_words("base.lst", ['ю', 'щ', 'я', 'ц', 'г'])))
+    print(check_user_words(
+        ['бабин', 'битий', 'бичий', 'білий', 'бісів', 'богів', 'божий', 'босий', 'булий', 'булів', 'бурий', 'ласий',
+         'лисий', 'литий', 'лихий', 'лівий', 'любий', 'лютий', 'усний', 'утлий', 'щирий', 'щучий', 'щучин'],
+        "adjective", ['ф', 'у', 'щ', 'б', 'л'], get_words("base.lst", ['ф', 'у', 'щ', 'б', 'л'])))
+    print(check_user_words([], "adverb", ['ш', 'ь', 'т', 'і', 'х'], get_words("base.lst", ['ш', 'ь', 'т', 'і', 'х'])))
